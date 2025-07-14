61a39b7c05546688ac32dd428c8aca3a08140886
mkhajiit <mkhajiit15@gmail.com>
2025-07-14 12:00:48 +0900

    feat: selenium 설치 확인완료

---
+++ tests/first.py
@@ -0,0 +1,11 @@
+# selenium 설치 테스트
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+driver.get("https://www.saucedemo.com")
+
+time.sleep(2)
