4518ab49567cfb5ca865d47f9d38cd443c0a95bb
mkhajiit <mkhajiit15@gmail.com>
2025-07-15 20:46:32 +0900

    feat:pytest 도입

--- tests/first.py
+++ tests/first.py
@@ -1,11 +1,17 @@
 # selenium 설치 테스트
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver.get("https://www.saucedemo.com")
+    yield driver
+    driver.quit()
 
-driver.get("https://www.saucedemo.com")
-
-time.sleep(2)
+def test_first(driver):
+  assert driver.current_url == "https://www.saucedemo.com/"
+  time.sleep(2)
