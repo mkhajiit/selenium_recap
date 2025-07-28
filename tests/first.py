596e98706857c4a0c30dc3d393a91c7b46e8f3f1
mkhajiit <mkhajiit15@gmail.com>
2025-07-28 22:32:12 +0900

    feat:로그인 복습

--- tests/first.py
+++ tests/first.py
@@ -2,6 +2,7 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 import time
 
@@ -12,6 +13,12 @@ def driver():
     yield driver
     driver.quit()
 
-def test_first(driver):
-  assert driver.current_url == "https://www.saucedemo.com/"
-  time.sleep(2)
+def test_login(driver):
+   driver.find_element(By.ID,'user-name').send_keys("standard_user")
+   driver.find_element(By.ID,'password').send_keys("secret_sauce")
+
+   driver.find_element(By.ID,"login-button").click()
+
+   time.sleep(2)
+
+   assert driver.current_url == "https://www.saucedemo.com/inventory.html"
