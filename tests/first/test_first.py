bda91e7be14f1e2b84a90e8e18aa17c5aff64784
mkhajiit <mkhajiit15@gmail.com>
2025-08-17 21:29:02 +0900

    feat: CSS_SELECTOR 연습

--- tests/first/test_first.py
+++ tests/first/test_first.py
@@ -16,7 +16,23 @@ def test_login(driver):
    # 로그인 후 로고가 화면에 존재하는지 판단
    assert logo.is_displayed()
 
-def test_xpath(driver):
-    driver.find_element(By.XPATH,"//input[contains(@class, 'input_error') and contains(@class, 'form_input')]").send_keys("standard_user")
-    # //input[...] 모든 input 태그에서 조건 만족하는 요소를 찾기
-    time.sleep(2)
+def test_by_class(driver):
+   username_input = driver.find_element(By.CSS_SELECTOR,'input[placeholder="Username"]')
+   username_input.send_keys("standard_user")
+
+   password_input = driver.find_element(By.CSS_SELECTOR, '.login-box input[placeholder="Password"]')
+   password_input.send_keys("secret_sauce")
+
+   driver.find_element(By.CSS_SELECTOR,'.login-box .submit-button.btn_action').click()
+   wait = WebDriverWait(driver, 10)
+   logo = wait.until(EC.presence_of_element_located((By.CLASS_NAME, "app_logo")))
+
+   time.sleep(2)
+
+   assert logo.is_displayed()
+   
+
+# def test_xpath(driver):
+#     driver.find_element(By.XPATH,"//input[contains(@class, 'input_error') and contains(@class, 'form_input')]").send_keys("standard_user")
+#     # //input[...] 모든 input 태그에서 조건 만족하는 요소를 찾기
+#     time.sleep(2)
