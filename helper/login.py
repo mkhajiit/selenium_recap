55815f90fec1abf0bba151838fd47d923dd41728
mkhajiit <mkhajiit15@gmail.com>
2025-08-21 00:19:05 +0900

    fix: 스크롤이 제대로 작동하는지 확인

--- helper/login.py
+++ helper/login.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def login(driver,username,password):
   driver.find_element(By.ID,'user-name').send_keys(username)
--- tests/third/test_third.py
+++ tests/third/test_third.py
@@ -5,5 +5,19 @@ from helper.login import login
 
 
 def test_third_scroll(driver):
-    login(driver,"standard_user","secret_sauce")
+  login(driver,"standard_user","secret_sauce")
+  print("로그인 함수 완료")
+
+  # 현재 스크롤 위치 확인
+  current_scroll = driver.execute_script("return window.scrollY;")
+
+  # 맨 아래로 스크롤
+  driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+  print("스크롤 실행 됨")
+
+  # 스크롤 후 위치 확인
+  new_scroll = driver.execute_script("return window.scrollY;")
+
+  assert new_scroll > current_scroll, "스크롤이 이동하지 않았습니다."
     
